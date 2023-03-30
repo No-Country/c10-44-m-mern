d60f90eb97f58e1113e3245f9974e3adc25580cd
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-03-30 17:31:24 -0600

    Fix: Refactoriza conexión a db usando URI de conexión

--- api/src/config/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const { DB_HOST, DB_PORT, DB_NAME } = process.env;
-
-export const db = mongoose.createConnection(`mongo://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
-
-export default db;
