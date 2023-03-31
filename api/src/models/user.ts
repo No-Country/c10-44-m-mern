7c7fa559c19c860c8e6a267f8a8baace60341a65
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-03-31 12:24:18 -0600

    backend/feat: Agrega estrategias JWT y local (#15)
    * feat: Agrega modelos de User y Role
    
    * feat: Agrega objeto de conexión a la base de datos
    
    * Renombra modelos usando mayúsculas
    
    * Agrega configuración de estrategia local para passport
    
    * feat: Agrega middleware para validar autenticación
    
    * feat: Agrega estrategia de autenticación con JWT

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,15 +1,63 @@
-import { Schema } from "mongoose";
-import db from "@/config/db";
+import { type Model, Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
-export const User = new Schema({
+/**
+ * Interface to model the User Schema
+ */
+interface IUser {
+  email: string;
+  passwordHash?: string;
+  displayName: string;
+  firstName?: string;
+  lastName?: string;
+  isPremium: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/**
+ * Interface to model the methods of the User Schema
+ */
+interface IUserMethods {
+  validatePassword: (candidatePassword: string) => Promise<boolean>;
+}
+
+/**
+ * The return type of the User model
+ */
+type UserModel = Model<IUser, {}, IUserMethods>;
+
+export const userSchema = new Schema<IUser, UserModel, IUserMethods>({
   email: { type: String, unique: true, required: true },
   passwordHash: String,
   displayName: { type: String, required: true },
   firstName: String,
   lastName: String,
-  isPremium: Boolean,
-  createdAt: Date,
-  updatedAt: Date,
+  isPremium: {type: Boolean, default: false},
+}, {
+  timestamps: true
 });
 
-export const UserModel = db.model("User", User);
+export type UserDocument = typeof userSchema;
+
+// Hash user password before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("passwordHash")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.passwordHash!, salt);
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
+
+// To compare incoming password with the hashed password in the database
+userSchema.method('validatePassword', async function (candidatePassword: string) {
+  return bcrypt.compare(candidatePassword, this.passwordHash);
+});
+
+export const User = model<IUser, UserModel>("User", userSchema);
