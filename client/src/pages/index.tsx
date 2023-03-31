ad7855460f4a3eac7b94df1e674cc9626774bc80
cereceda1991 <cereceda1991@gmail.com>
2023-03-31 11:56:17 -0500

    Creacion de  BenefitsSection

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Benne, Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import BenefitsSection from '@/components/BenefitsSection'
+import HomeFooter from '@/components/HomeFooter'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <BenefitsSection/>
+      <HomeFooter/>
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
