0307e3a22ba20cb2d7cdc408d94765273bb50ab1
Carlos Aguilera <ckarlos_95@hotmail.es>
2023-03-31 12:08:05 -0600

    Fix: Refactoriza conexión a db usando URI de conexión (#14)
    Fix: Refactorizacion de conexion de DB con URI

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,26 @@
 import express, { Express } from "express";
+import mongoose from "mongoose"
 import * as dotenv from "dotenv";
+import { exit } from "process";
 dotenv.config();
+
+// Mongoose initialization
+try {
+  if (process.env.MONGO_URI === undefined) {
+    throw new Error("Set the MONGO_URI environment variable");
+  }
+
+  mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB connected");
+} catch (error) {
+  console.error("MongoDB connection error");
+  
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
+  
+  exit(1);
+}
 
 //instance of express
 const app: Express = express();
