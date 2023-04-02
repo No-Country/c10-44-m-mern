38dafd7a6bee64fef923cc21b75b19c9c64d3eb5
cereceda1991 <cereceda1991@gmail.com>
2023-04-02 14:15:08 -0500

    Revert " Deshacer Correción de un bug, importacion duplicada"
    This reverts commit 4fed6c04a41b3639e7cc0fed19081a186ec02652.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
+import Image from 'next/image'
 
 import NavBar from '@/components/NavBar'
 import HomeHero from '@/components/HomeHero'
