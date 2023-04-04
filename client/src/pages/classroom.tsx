cba93933e5055a265abc684a6e378d77c02db9cf
Lucio Vasquez <castrocociolucio@gmail.com>
2023-04-04 17:57:55 -0300

    Adding video box to Classroom page

--- client/src/pages/classroom.tsx
+++ client/src/pages/classroom.tsx
@@ -1,14 +1,18 @@
 import UnitsBox from "@/components/UnitsBox";
+import VideoBox from "@/components/VideoBox";
 
 const unit = {
   title: "U1: Como será estudiar en verbify",
-  theme: ["Ejemplo de tema 1", "Ejemplo de tema 2", "Ejemplo de tema 3"]
-}
+  theme: ["Ejemplo de tema 1", "Ejemplo de tema 2", "Ejemplo de tema 3"],
+};
 
 function ClassRoom() {
   return (
-    <UnitsBox title={unit.title} themes={unit.theme} />
-  )
+    <div>
+      <VideoBox videourl="" />
+      <UnitsBox title={unit.title} themes={unit.theme} />
+    </div>
+  );
 }
 
 export default ClassRoom;
