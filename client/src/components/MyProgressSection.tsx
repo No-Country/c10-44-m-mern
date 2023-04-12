782d874bb2b4b2f839fe50c53c9470f07e719478
cereceda1991 <cereceda1991@gmail.com>
2023-04-12 16:07:48 -0500

    Actualizacion de estilos en mi progreso

--- client/src/components/MyProgressSection.tsx
+++ client/src/components/MyProgressSection.tsx
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { BiLock, BiLockOpen } from 'react-icons/bi';
-import styles from '../styles/myprogress.module.css';
+import { BiCaretUp, BiLock, BiLockOpen } from "react-icons/bi";
+import styles from "../styles/myprogress.module.css";
 
 interface Section {
-    title: string;
-    content: React.ReactNode[];
+  title: string;
+  content: React.ReactNode[];
 }
 
 function MyProgressSection(props: Section) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
-    const [isCollapsed, setIsCollapsed] = useState(true);
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
-    const toggleCollapse = () => {
-        setIsCollapsed(!isCollapsed);
-    };
-
-    return (
-        <section >
-            <div className={styles.container__section}>
-                <p>{props.title}</p>
-                <button onClick={toggleCollapse}>{isCollapsed ? <BiLock />
-                    : <BiLockOpen />
-                }</button>
+  return (
+    <section>
+      <div
+        className={`${styles.container__section} ${
+          isCollapsed ? "" : styles.collapsed
+        }`}
+      >
+        <p>{props.title}</p>
+        <button onClick={toggleCollapse}>
+          {isCollapsed ? (
+            <BiLock />
+          ) : (
+            <div className={styles.cardCourse__Count}>
+              <h5>0/1</h5> <BiCaretUp />
             </div>
-            <div className={styles.container__course}>
-                {!isCollapsed && props.content.map((item, index) => (
-                    <p key={index}>{item}</p>
-                ))}
-            </div>
-        </section>
-    );
+          )}
+        </button>
+      </div>
+      <div className={styles.container__course}>
+        {!isCollapsed &&
+          props.content.map((item, index) => <p key={index}>{item}</p>)}
+      </div>
+    </section>
+  );
 }
 
-
-export default MyProgressSection
+export default MyProgressSection;
