9cf5b13a729f03554ac2313f49b9e0ff1d251665
cereceda1991 <cereceda1991@gmail.com>
2023-04-16 02:48:42 -0500

    Se agregó el responsive a mi progreso

--- client/src/components/SideBarMobile.tsx
+++ client/src/components/SideBarMobile.tsx
@@ -6,10 +6,10 @@ import {
   BiBookContent,
   BiPencil,
   BiLogOut,
-  BiMenu,
+  BiRightArrowAlt,
 } from "react-icons/bi";
+
 import Avatar from "../assets/Avatar.png";
-import VerbifyLogo from "../assets/Verbify-logo.png";
 import iconV from "../assets/Icon_verbify.png";
 
 function SideBarMobile() {
@@ -21,39 +21,30 @@ function SideBarMobile() {
 
   return (
     <nav className={styles.container_sidebarmobile}>
-      <div onClick={toggleMenu}>
-        {showMenu ? (
-          <>
-            <div className={styles.menu_mobile}>
-              <img src={iconV.src} />
-              <p>Menu</p>
-              <hr />
-              <button>
-                <BiHomeAlt size={28} />
-              </button>
-              <button>
-                <BiUserCircle size={28} />
-              </button>
-              <button>
-                <BiBookContent size={28} />
-              </button>
-              <button>
-                <BiPencil size={28} />
-              </button>
-              <hr />
-              <img src={Avatar.src} />
-              <button>
-                <BiLogOut size={28} />
-              </button>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className={styles.icon_sandwich}>
-              <BiMenu />
-            </div>
-          </>
-        )}
+      <div className={`${styles.menu_mobile} ${showMenu ? "" : styles.hidden}`}>
+        <img src={iconV.src} onClick={toggleMenu} />
+        <p>Menu</p>
+        <hr />
+        <button>
+          <BiHomeAlt size={28} />
+        </button>
+        <button>
+          <BiUserCircle size={28} />
+        </button>
+        <button>
+          <BiBookContent size={28} />
+        </button>
+        <button>
+          <BiPencil size={28} />
+        </button>
+        <hr />
+        <img src={Avatar.src} />
+        <button>
+          <BiLogOut size={28} />
+        </button>
+      </div>
+      <div className={styles.icon_RightArrow} onClick={toggleMenu}>
+        <BiRightArrowAlt />
       </div>
     </nav>
   );
