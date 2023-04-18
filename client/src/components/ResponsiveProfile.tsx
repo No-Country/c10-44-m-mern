6769d6a1fab4ffe6f199df76e87d114cdb02fc22
Lucio Vasquez <castrocociolucio@gmail.com>
2023-04-18 17:03:35 -0300

    Añadido el responsive la profile page

---
+++ client/src/components/ResponsiveProfile.tsx
@@ -0,0 +1,106 @@
+import SideBarMobile from "./SideBarMobile";
+import styles from "@/styles/ResponsiveProfile.module.css";
+import Avatar from "@/assets/Avatar.png";
+import { useAppSelector } from "@/store/hooks";
+
+function ResponsiveProfile() {
+  const userData = {
+    image: Avatar.src,
+    name: "Robert",
+    surname: "Gomez",
+    occupation: "Ingeniero",
+    email: "correo@gmail.com",
+    number: "+00 346 46 46",
+    bio: "Ingeniero",
+    country: "Chile",
+    product: "Plan Anual",
+    expirationDate: "06/04/2024",
+  };
+  const { authList } = useAppSelector((rootReducer) => rootReducer.auth);
+
+  return (
+    <div className={styles.background}>
+      <SideBarMobile />
+      <section className={styles.info}>
+        <h1 className={styles.title}>Mi perfil</h1>
+        <div className={styles.alluserdata}>
+          <section className={styles.section}>
+            <div className={styles.userdetails}>
+              <div className={styles.imagebox}>
+                <img
+                  className={styles.image}
+                  src={userData.image}
+                  alt="user image"
+                />
+              </div>
+              <div className={styles.shortdescription}>
+                <p className={styles.userName}>
+                  {authList?.firstName} {authList?.lastName}
+                </p>
+                <p>{userData.country}</p>
+                <p>{userData.occupation}</p>
+              </div>
+            </div>
+          </section>
+          <section className={styles.section}>
+            <div>
+              <p className={styles.userName}>Información Personal</p>
+              <div className={styles.container}>
+                <div className={styles.column}>
+                  <div>
+                    <p className={styles.label}>Nombre</p>
+                    <p>{authList?.firstName}</p>
+                  </div>
+                  <div>
+                    <p className={styles.label}>Correo Electrónico</p>
+                    <p>{authList?.email}</p>
+                  </div>
+                  <div>
+                    <p className={styles.label}>Bio</p>
+                    <p>{userData.occupation}</p>
+                  </div>
+                </div>
+                <div className={styles.column}>
+                  <div>
+                    <p className={styles.label}>Apellido</p>
+                    <p>{authList?.lastName}</p>
+                  </div>
+                  <div>
+                    <p className={styles.label}>Teléfono</p>
+                    <p>{userData.number}</p>
+                  </div>
+                  <div>
+                    <p className={styles.label}>País</p>
+                    <p>{userData.country}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className={styles.section}>
+            <div>
+              <p>Producto y método de pago</p>
+              <div className={styles.payment}>
+                <div>
+                  <p className={styles.label}>Producto</p>
+                  <p>{userData.product}</p>
+                </div>
+                <div>
+                  <p className={styles.label}>Vence</p>
+                  <p>{userData.expirationDate}</p>
+                </div>
+                <div>
+                  <p className={styles.label}>Número de tarjeta</p>
+                  <p>numerodsadeadwa</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+        <button className={styles.deletebutton}>Eliminar cuenta</button>
+      </section>
+    </div>
+  );
+}
+
+export default ResponsiveProfile;
--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -2,8 +2,12 @@ import SideBar from "@/components/SideBar";
 import styles from "@/styles/Profile.module.css";
 import Avatar from "@/assets/Avatar.png";
 import { useAppSelector } from "@/store/hooks";
+import React, { ReactNode } from "react";
+import ResponsiveProfile from "@/components/ResponsiveProfile";
 
 function ProfilePage() {
+  const [width, setWidth] = React.useState(0);
+  const breakpoint = 860;
   const userData = {
     image: Avatar.src,
     name: "Robert",
@@ -17,95 +21,108 @@ function ProfilePage() {
     expirationDate: "06/04/2024",
   };
 
-  const {
-    authList
-  } = useAppSelector(rootReducer => rootReducer.auth)
+  React.useEffect(() => {
+    const handleWindowResize = () => {
+      setWidth(window.innerWidth);
+    };
+    handleWindowResize();
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
 
-  return (
-    <div className={styles.background}>
-      <SideBar />
-      <section className={styles.info}>
-        <h1 className={styles.title}>Mi perfil</h1>
-        <div className={styles.alluserdata}>
-          <section className={styles.section}>
-            <div className={styles.userdetails}>
-              <div className={styles.imagebox}>
-                <img
-                  className={styles.image}
-                  src={userData.image}
-                  alt="user image"
-                />
-              </div>
-              <div className={styles.shortdescription}>
-                <p className={styles.userName}>
-                  {authList?.firstName} {authList?.lastName}
-                </p>
-                <p>{userData.country}</p>
-                <p>{userData.occupation}</p>
-              </div>
-            </div>
-            <button className={styles.editbutton}>Editar</button>
-          </section>
-          <section className={styles.section}>
-            <div>
-              <p className={styles.userName}>Información Personal</p>
-              <div className={styles.container}>
-                <div className={styles.column}>
-                  <div>
-                    <p className={styles.label}>Nombre</p>
-                    <p>{authList?.firstName}</p>
-                  </div>
-                  <div>
-                    <p className={styles.label}>Correo Electrónico</p>
-                    <p>{authList?.email}</p>
-                  </div>
-                  <div>
-                    <p className={styles.label}>Bio</p>
-                    <p>{userData.occupation}</p>
-                  </div>
+  const { authList } = useAppSelector((rootReducer) => rootReducer.auth);
+
+  return width > 0 ? (
+    width < breakpoint ? (
+      <ResponsiveProfile />
+    ) : (
+      <div className={styles.background}>
+        <SideBar />
+        <section className={styles.info}>
+          <h1 className={styles.title}>Mi perfil</h1>
+          <div className={styles.alluserdata}>
+            <section className={styles.section}>
+              <div className={styles.userdetails}>
+                <div className={styles.imagebox}>
+                  <img
+                    className={styles.image}
+                    src={userData.image}
+                    alt="user image"
+                  />
                 </div>
-                <div className={styles.column}>
-                  <div>
-                    <p className={styles.label}>Apellido</p>
-                    <p>{authList?.lastName}</p>
-                  </div>
-                  <div>
-                    <p className={styles.label}>Teléfono</p>
-                    <p>{userData.number}</p>
-                  </div>
-                  <div>
-                    <p className={styles.label}>País</p>
-                    <p>{userData.country}</p>
-                  </div>
+                <div className={styles.shortdescription}>
+                  <p className={styles.userName}>
+                    {authList?.firstName} {authList?.lastName}
+                  </p>
+                  <p>{userData.country}</p>
+                  <p>{userData.occupation}</p>
                 </div>
               </div>
-            </div>
-            <button className={styles.editbutton}>Editar</button>
-          </section>
-          <section className={styles.section}>
-            <div>
-              <p>Producto y método de pago</p>
-              <div className={styles.payment}>
-                <div>
-                  <p className={styles.label}>Producto</p>
-                  <p>{userData.product}</p>
-                </div>
-                <div>
-                  <p className={styles.label}>Vence</p>
-                  <p>{userData.expirationDate}</p>
-                </div>
-                <div>
-                  <p className={styles.label}>Número de tarjeta</p>
-                  <p>numerodsadeadwa</p>
+              <button className={styles.editbutton}>Editar</button>
+            </section>
+            <section className={styles.section}>
+              <div>
+                <p className={styles.userName}>Información Personal</p>
+                <div className={styles.container}>
+                  <div className={styles.column}>
+                    <div>
+                      <p className={styles.label}>Nombre</p>
+                      <p>{authList?.firstName}</p>
+                    </div>
+                    <div>
+                      <p className={styles.label}>Correo Electrónico</p>
+                      <p>{authList?.email}</p>
+                    </div>
+                    <div>
+                      <p className={styles.label}>Bio</p>
+                      <p>{userData.occupation}</p>
+                    </div>
+                  </div>
+                  <div className={styles.column}>
+                    <div>
+                      <p className={styles.label}>Apellido</p>
+                      <p>{authList?.lastName}</p>
+                    </div>
+                    <div>
+                      <p className={styles.label}>Teléfono</p>
+                      <p>{userData.number}</p>
+                    </div>
+                    <div>
+                      <p className={styles.label}>País</p>
+                      <p>{userData.country}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <button className={styles.editbutton}>Editar</button>
-          </section>
-        </div>
-        <button className={styles.deletebutton}>Eliminar cuenta</button>
-      </section>
-    </div>
+              <button className={styles.editbutton}>Editar</button>
+            </section>
+            <section className={styles.section}>
+              <div>
+                <p>Producto y método de pago</p>
+                <div className={styles.payment}>
+                  <div>
+                    <p className={styles.label}>Producto</p>
+                    <p>{userData.product}</p>
+                  </div>
+                  <div>
+                    <p className={styles.label}>Vence</p>
+                    <p>{userData.expirationDate}</p>
+                  </div>
+                  <div>
+                    <p className={styles.label}>Número de tarjeta</p>
+                    <p>numerodsadeadwa</p>
+                  </div>
+                </div>
+              </div>
+              <button className={styles.editbutton}>Editar</button>
+            </section>
+          </div>
+          <button className={styles.deletebutton}>Eliminar cuenta</button>
+        </section>
+      </div>
+    )
+  ) : (
+    <div className={styles.div}>Cargando...</div>
   );
 }
 
