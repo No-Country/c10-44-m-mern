a31d34a018cd9ce21c278a9f77265cb4f8ad5181
cereceda1991 <cereceda1991@gmail.com>
2023-04-19 22:48:18 -0500

    Cree el responsive de la pagina de error

--- client/src/pages/_error.tsx
+++ client/src/pages/_error.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import styles from '../styles/Page404.module.css';
+import React from "react";
+import styles from "../styles/Page404.module.css";
 
 //Resources
-import logo from '../assets/Verbify-logo.svg';
-import Group from '../assets/Group.svg';
-import img404 from '../assets/404.svg'
+import logo from "../assets/Verbify-logo.svg";
+import Group from "../assets/Group.svg";
+import img404 from "../assets/404.svg";
 
-function Page404 () {
+import { useRouter } from "next/router";
+
+function Page404() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
     <div className={styles.container__Page404}>
-        <header>
-    <img src={logo.src}/>
-    </header>
-    <section>
-      <div>
-        <img src={img404.src}/>
-        <p>¡Ups! Esta Pagina no esta disponible por el momento</p>
-        <button>Pagina anterior</button>
-      </div>
-      <div>
-        <img src={Group.src}/>
-      </div>
-      </section>  
+      <header>
+        <img src={logo.src} />
+      </header>
+      <section>
+        <div>
+          <img src={img404.src} />
+          <p>¡Ups! Esta Pagina no esta disponible por el momento</p>
+          <button onClick={handleGoBack}>Pagina anterior</button>
+        </div>
+        <div>
+          <img src={Group.src} />
+        </div>
+      </section>
     </div>
   );
 }
